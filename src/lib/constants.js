15ec191bd48cc6c0ae061b8786a4c30614721cc9
riCl3 <soumyaric2@gmail.com>
2026-07-03 19:13:27 +0530

    fix: use decimal CF7 post ID (8992) and add required _wpcf7_unit_tag

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_ENDPOINT = 'https://mksales.co.in/graphql'
-export const CF7_FEEDBACK_URL = 'https://mksales.co.in/wp-json/contact-form-7/v1/contact-forms/2320/feedback'
+export const CF7_FEEDBACK_URL = 'https://mksales.co.in/wp-json/contact-form-7/v1/contact-forms/8992/feedback'
