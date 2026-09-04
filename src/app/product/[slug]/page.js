a95bf7d89871ce19a78926ce35f423afae3ad4cd
riCl3 <soumyaric2@gmail.com>
2026-09-04 16:21:09 +0530

    fix: reduce ISR revalidate to 60s for product pages to reflect WP updates faster

--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -34,7 +34,7 @@ async function getProduct(slug) {
     const res = await fetch(GRAPHQL_ENDPOINT, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      next: { revalidate: 3600 },
+      next: { revalidate: 60 },
       body: JSON.stringify({ query, variables: { id: productSlug } }),
     });
 
@@ -46,7 +46,7 @@ async function getProduct(slug) {
   }
 }
 
-export const revalidate = 3600;
+export const revalidate = 60;
 
 export default async function ProductPage({ params }) {
   const { slug } = await params;
